77e4a3bc6ce762858855a8b5654945850b475678
Christopher Stuart <63csmith@gmail.com>
2024-07-10 22:35:33 -0600

    duck loco
    added the script of duck player to move node 2d location to the parent node

--- duck_player.gd
+++ duck_player.gd
@@ -1,18 +1,25 @@
 extends CharacterBody2D
 
-
-
-signal health_depleted 
+signal health_depleted
 
 var health = 100.0
 var can_move = true
+
+func _ready():
+	print("Initial Duck_player global position: ", global_position)
+	print("Initial Ducky global position: ", $Ducky.global_position)
+	print("Initial CollisionShape2D position: ", $CollisionShape2D.position)
 
 func _physics_process(delta):
 	if can_move:
 		var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 		velocity = direction * 600
 		move_and_slide()
-		
+
+		print("Duck_player position: ", position)
+		print("Duck_player global position: ", global_position)
+		print("Ducky global position: ", $Ducky.global_position)
+
 		if velocity.length() > 0.0:
 			$Ducky.play_duck_walk_animation()
 		else:
@@ -29,3 +36,6 @@ func _physics_process(delta):
 	else:
 		velocity = Vector2.ZERO  # Ensure the player doesn't move
 		move_and_slide()  # Call this to apply the zero velocity
+	
+	# Manually update Ducky's position to follow Duck_player
+	$Ducky.position = position
